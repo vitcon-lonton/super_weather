eb16205a898b80ea0e4f2fee7c8151101701c641
vit con lon ton <hoanghiep97@hotmail.com>
2023-11-11 15:54:09 +0700

    Refactor app

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,41 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geocoding_repository/geocoding_repository.dart';
-import 'package:open_meteo_api_client/open_meteo_api_client.dart';
 import 'package:super_weather/l10n/l10n.dart';
 import 'package:super_weather/theme/theme.dart';
 import 'package:super_weather/weather/weather.dart';
 import 'package:weather_repository/weather_repository.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  late final OpenMeteoApiClient openMeteoClient;
-
-  @override
-  void initState() {
-    super.initState();
-    openMeteoClient = OpenMeteoApiClient();
-  }
 
   @override
   Widget build(BuildContext context) {
     return MultiRepositoryProvider(
       providers: [
-        RepositoryProvider(
-          create: (_) => GeocodingRepository(openMeteoClient: openMeteoClient),
-        ),
-        RepositoryProvider(
-          create: (_) => WeatherRepository(openMeteoClient: openMeteoClient),
-        ),
+        RepositoryProvider(create: (_) => GeocodingRepository()),
+        RepositoryProvider(create: (_) => WeatherRepository()),
       ],
-      child: MultiBlocProvider(
-        providers: [BlocProvider(create: (_) => ThemeCubit())],
+      child: BlocProvider(
+        create: (_) => ThemeCubit(),
         child: const AppView(),
       ),
     );
