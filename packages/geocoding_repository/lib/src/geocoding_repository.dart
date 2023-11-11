ecd0d74e3ec5944b2cf15fefa1617277c9dd71f7
vit con lon ton <hoanghiep97@hotmail.com>
2023-11-11 15:53:07 +0700

    Refactor geocoding repo, add unit test

--- packages/geocoding_repository/lib/src/geocoding_repository.dart
+++ packages/geocoding_repository/lib/src/geocoding_repository.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:geocoding_repository/geocoding_repository.dart';
-import 'package:open_meteo_api_client/open_meteo_api_client.dart' hide Location;
+import 'package:open_meteo_api_client/open_meteo_api_client.dart' as meteo;
 
 /// {@template geocoding_repository}
 /// A Dart Repository which manages the geocoding domain
@@ -9,26 +9,29 @@ import 'package:open_meteo_api_client/open_meteo_api_client.dart' hide Location;
 class GeocodingRepository {
   /// {@macro geocoding_repository}
   GeocodingRepository({
-    OpenMeteoApiClient? openMeteoClient,
-  }) : _meteoClient = openMeteoClient ?? OpenMeteoApiClient();
+    meteo.GeocodingApi? geocodingApi,
+  }) : _geocodingApi = geocodingApi ?? meteo.GeocodingApi.initialize();
 
-  final OpenMeteoApiClient _meteoClient;
+  final meteo.GeocodingApi _geocodingApi;
 
   /// Method to search geocoding from keyword.
   Future<List<Location>> search(String name) async {
-    final response = await _meteoClient.geocoding.search(count: 30, name: name);
-    final locations = response.results?.map((element) {
-          return Location(
-            id: element.id,
-            name: element.name,
-            latitude: element.latitude,
-            longitude: element.longitude,
-            countryId: element.countryId,
-            country: element.country,
-          );
-        }).toList() ??
-        List<Location>.empty();
+    final query = meteo.SearchQuery(name, count: 30);
+    final response = await _geocodingApi.search(query);
+    final locations = response.results?.map((e) => e.toDomain()).toList();
+    return locations ?? List<Location>.empty();
+  }
+}
 
-    return locations;
+extension on meteo.Location {
+  Location toDomain() {
+    return Location(
+      id: id,
+      name: name,
+      latitude: latitude,
+      longitude: longitude,
+      countryId: countryId,
+      country: country,
+    );
   }
 }
