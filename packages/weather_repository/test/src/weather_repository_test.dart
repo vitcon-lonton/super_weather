67f2e5db6eb0182832eea55bcd360897de98606b
vit con lon ton <hoanghiep97@hotmail.com>
2023-11-12 17:53:57 +0700

    Refactor weather repo, add unit test

--- packages/weather_repository/test/src/weather_repository_test.dart
+++ packages/weather_repository/test/src/weather_repository_test.dart
@@ -1,231 +1,198 @@
-// import 'package:mocktail/mocktail.dart';
-// import 'package:open_meteo_api_client/open_meteo_api_client.dart' as open_meteo;
-// import 'package:test/test.dart';
-// import 'package:weather_repository/weather_repository.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:open_meteo_api_client/open_meteo_api_client.dart' as meteo;
+import 'package:test/test.dart';
+import 'package:weather_repository/weather_repository.dart';
 
-// class MockWeatherApi extends Mock implements open_meteo.WeatherApi {}
+class MockWeatherApi extends Mock implements meteo.WeatherApi {}
 
-// class MockWeather extends Mock implements open_meteo.Weather {}
+class FakeWeatherQuery extends Fake implements meteo.WeatherQuery {}
 
-// class MockCurrentWeather extends Mock implements open_meteo.CurrentWeather {}
+class MockWeather extends Mock implements meteo.Weather {}
 
-// void main() {
-//   group('WeatherRepository', () {
-//     late open_meteo.WeatherApi weatherApi;
-//     late WeatherRepository weatherRepository;
+class MockCurrentWeather extends Mock implements meteo.CurrentWeather {}
 
-//     setUp(() {
-//       weatherApi = MockWeatherApi();
-//       weatherRepository = WeatherRepository(weatherApi: weatherApi);
-//     });
+void main() {
+  group('WeatherRepository', () {
+    late meteo.WeatherApi weatherApi;
+    late WeatherRepository weatherRepository;
 
-//     group('constructor', () {
-//       test('instantiates internal weather api client when not injected', () {
-//         expect(WeatherRepository(), isNotNull);
-//       });
-//     });
+    setUp(() {
+      weatherApi = MockWeatherApi();
+      weatherRepository = WeatherRepository(weatherApi: weatherApi);
+    });
 
-//     group('getWeather', () {
-//       const latitude = 41.85003;
-//       const longitude = -87.65005;
-//       const elevation = 12.0;
-//       const temperature = 42.42;
-//       const weathercode = 2;
+    setUpAll(() {
+      registerFallbackValue(FakeWeatherQuery());
+    });
 
-//       test('calls getWeather with correct latitude/longitude', () async {
-//         final weather = MockWeather();
-//         final currentWeather = MockCurrentWeather();
+    group('constructor', () {
+      test('instantiates internal weather api client when not injected', () {
+        expect(WeatherRepository(), isNotNull);
+      });
+    });
 
-//         when(() => currentWeather.temperature).thenReturn(temperature);
-//         when(() => currentWeather.weathercode).thenReturn(weathercode);
-//         when(() => weather.latitude).thenReturn(latitude);
-//         when(() => weather.longitude).thenReturn(longitude);
-//         when(() => weather.elevation).thenReturn(elevation);
-//         when(() => weather.currentWeather).thenReturn(currentWeather);
-//         when(
-//           () => weatherApi.getWeather(
-//             lat: any(named: 'lat'),
-//             long: any(named: 'long'),
-//           ),
-//         ).thenAnswer((_) async => weather);
+    group('getWeather', () {
+      const latitude = 41.85003;
+      const longitude = -87.65005;
+      const elevation = 12.0;
+      const temperature = 42.42;
 
-//         try {
-//           await weatherRepository.getWeather(
-//               latitude: latitude, longitude: longitude);
-//         } catch (_) {}
+      test('calls getWeather with correct latitude/longitude', () async {
+        try {
+          await weatherRepository.getWeather(
+              latitude: latitude, longitude: longitude);
+        } catch (_) {}
 
-//         verify(
-//           () => weatherApi.getWeather(lat: latitude, long: longitude),
-//         ).called(1);
-//       });
+        final matcher = isA<meteo.WeatherQuery>()
+            .having((w) => w.latitude, 'latitude', latitude)
+            .having((w) => w.longitude, 'longitude', longitude);
 
-//       test('throws when getWeather fails', () async {
-//         final exception = Exception('oops');
+        verify(() => weatherApi.getWeather(any(that: matcher))).called(1);
+      });
 
-//         when(
-//           () => weatherApi.getWeather(
-//             lat: any(named: 'lat'),
-//             long: any(named: 'long'),
-//           ),
-//         ).thenThrow(exception);
+      test('throws when getWeather fails', () async {
+        final exception = Exception('oops');
 
-//         expect(
-//           () async => weatherRepository.getWeather(
-//             latitude: latitude,
-//             longitude: longitude,
-//           ),
-//           throwsA(exception),
-//         );
-//       });
-//       test('returns correct weather on success (unknown)', () async {
-//         final weather = MockWeather();
-//         final currentWeather = MockCurrentWeather();
-//         when(() => currentWeather.temperature).thenReturn(temperature);
-//         when(() => currentWeather.weathercode).thenReturn(-1);
-//         when(() => weather.latitude).thenReturn(latitude);
-//         when(() => weather.longitude).thenReturn(longitude);
-//         when(() => weather.elevation).thenReturn(elevation);
-//         when(() => weather.currentWeather).thenReturn(currentWeather);
-//         when(
-//           () => weatherApi.getWeather(
-//             lat: any(named: 'lat'),
-//             long: any(named: 'long'),
-//           ),
-//         ).thenAnswer((_) async => weather);
+        when(() => weatherApi.getWeather(any())).thenThrow(exception);
 
-//         final actual = await weatherRepository.getWeather(
-//             latitude: latitude, longitude: longitude);
+        expect(
+          weatherRepository.getWeather(
+            latitude: latitude,
+            longitude: longitude,
+          ),
+          throwsA(exception),
+        );
+      });
 
-//         expect(
-//           actual,
-//           const Weather(
-//             temperature: temperature,
-//             condition: WeatherCondition.unknown,
-//             latitude: latitude,
-//             longitude: longitude,
-//           ),
-//         );
-//       });
+      test('returns correct weather on success (unknown)', () async {
+        final weather = MockWeather();
+        final currentWeather = MockCurrentWeather();
+        when(() => currentWeather.temperature).thenReturn(temperature);
+        when(() => currentWeather.weathercode).thenReturn(-1);
+        when(() => weather.latitude).thenReturn(latitude);
+        when(() => weather.longitude).thenReturn(longitude);
+        when(() => weather.elevation).thenReturn(elevation);
+        when(() => weather.currentWeather).thenReturn(currentWeather);
+        when(() => weatherApi.getWeather(any()))
+            .thenAnswer((_) async => weather);
 
-//       test('returns correct weather on success (snowy)', () async {
-//         final weather = MockWeather();
-//         final currentWeather = MockCurrentWeather();
-//         when(() => currentWeather.temperature).thenReturn(temperature);
-//         when(() => currentWeather.weathercode).thenReturn(71);
-//         when(() => weather.latitude).thenReturn(latitude);
-//         when(() => weather.longitude).thenReturn(longitude);
-//         when(() => weather.elevation).thenReturn(elevation);
-//         when(() => weather.currentWeather).thenReturn(currentWeather);
-//         when(
-//           () => weatherApi.getWeather(
-//             lat: any(named: 'lat'),
-//             long: any(named: 'long'),
-//           ),
-//         ).thenAnswer((_) async => weather);
+        final actual = await weatherRepository.getWeather(
+            latitude: latitude, longitude: longitude);
 
-//         final actual = await weatherRepository.getWeather(
-//             latitude: latitude, longitude: longitude);
+        expect(
+          actual,
+          const Weather(
+            temperature: temperature,
+            condition: WeatherCondition.unknown,
+            latitude: latitude,
+            longitude: longitude,
+          ),
+        );
+      });
 
-//         expect(
-//           actual,
-//           const Weather(
-//             temperature: temperature,
-//             condition: WeatherCondition.snowy,
-//             latitude: latitude,
-//             longitude: longitude,
-//           ),
-//         );
-//       });
+      test('returns correct weather on success (snowy)', () async {
+        final weather = MockWeather();
+        final currentWeather = MockCurrentWeather();
+        when(() => currentWeather.temperature).thenReturn(temperature);
+        when(() => currentWeather.weathercode).thenReturn(71);
+        when(() => weather.latitude).thenReturn(latitude);
+        when(() => weather.longitude).thenReturn(longitude);
+        when(() => weather.elevation).thenReturn(elevation);
+        when(() => weather.currentWeather).thenReturn(currentWeather);
+        when(() => weatherApi.getWeather(any()))
+            .thenAnswer((_) async => weather);
 
-//       test('returns correct weather on success (rainy)', () async {
-//         final weather = MockWeather();
-//         final currentWeather = MockCurrentWeather();
-//         when(() => currentWeather.temperature).thenReturn(temperature);
-//         when(() => currentWeather.weathercode).thenReturn(51);
-//         when(() => weather.latitude).thenReturn(latitude);
-//         when(() => weather.longitude).thenReturn(longitude);
-//         when(() => weather.elevation).thenReturn(elevation);
-//         when(() => weather.currentWeather).thenReturn(currentWeather);
-//         when(
-//           () => weatherApi.getWeather(
-//             lat: any(named: 'lat'),
-//             long: any(named: 'long'),
-//           ),
-//         ).thenAnswer((_) async => weather);
+        final actual = await weatherRepository.getWeather(
+            latitude: latitude, longitude: longitude);
 
-//         final actual = await weatherRepository.getWeather(
-//             latitude: latitude, longitude: longitude);
+        expect(
+          actual,
+          const Weather(
+            temperature: temperature,
+            condition: WeatherCondition.snowy,
+            latitude: latitude,
+            longitude: longitude,
+          ),
+        );
+      });
 
-//         expect(
-//           actual,
-//           const Weather(
-//             temperature: temperature,
-//             condition: WeatherCondition.rainy,
-//             latitude: latitude,
-//             longitude: longitude,
-//           ),
-//         );
-//       });
+      test('returns correct weather on success (rainy)', () async {
+        final weather = MockWeather();
+        final currentWeather = MockCurrentWeather();
+        when(() => currentWeather.temperature).thenReturn(temperature);
+        when(() => currentWeather.weathercode).thenReturn(51);
+        when(() => weather.latitude).thenReturn(latitude);
+        when(() => weather.longitude).thenReturn(longitude);
+        when(() => weather.elevation).thenReturn(elevation);
+        when(() => weather.currentWeather).thenReturn(currentWeather);
+        when(() => weatherApi.getWeather(any()))
+            .thenAnswer((_) async => weather);
 
-//       test('returns correct weather on success (clear)', () async {
-//         final weather = MockWeather();
-//         final currentWeather = MockCurrentWeather();
-//         when(() => currentWeather.temperature).thenReturn(temperature);
-//         when(() => currentWeather.weathercode).thenReturn(0);
-//         when(() => weather.latitude).thenReturn(latitude);
-//         when(() => weather.longitude).thenReturn(longitude);
-//         when(() => weather.elevation).thenReturn(elevation);
-//         when(() => weather.currentWeather).thenReturn(currentWeather);
-//         when(
-//           () => weatherApi.getWeather(
-//             lat: any(named: 'lat'),
-//             long: any(named: 'long'),
-//           ),
-//         ).thenAnswer((_) async => weather);
+        final actual = await weatherRepository.getWeather(
+            latitude: latitude, longitude: longitude);
 
-//         final actual = await weatherRepository.getWeather(
-//             latitude: latitude, longitude: longitude);
+        expect(
+          actual,
+          const Weather(
+            temperature: temperature,
+            condition: WeatherCondition.rainy,
+            latitude: latitude,
+            longitude: longitude,
+          ),
+        );
+      });
 
-//         expect(
-//           actual,
-//           const Weather(
-//             temperature: temperature,
-//             condition: WeatherCondition.clear,
-//             latitude: latitude,
-//             longitude: longitude,
-//           ),
-//         );
-//       });
+      test('returns correct weather on success (clear)', () async {
+        final weather = MockWeather();
+        final currentWeather = MockCurrentWeather();
+        when(() => currentWeather.temperature).thenReturn(temperature);
+        when(() => currentWeather.weathercode).thenReturn(0);
+        when(() => weather.latitude).thenReturn(latitude);
+        when(() => weather.longitude).thenReturn(longitude);
+        when(() => weather.elevation).thenReturn(elevation);
+        when(() => weather.currentWeather).thenReturn(currentWeather);
+        when(() => weatherApi.getWeather(any()))
+            .thenAnswer((_) async => weather);
 
-//       test('returns correct weather on success (cloudy)', () async {
-//         final weather = MockWeather();
-//         final currentWeather = MockCurrentWeather();
-//         when(() => currentWeather.temperature).thenReturn(temperature);
-//         when(() => currentWeather.weathercode).thenReturn(2);
-//         when(() => weather.latitude).thenReturn(latitude);
-//         when(() => weather.longitude).thenReturn(longitude);
-//         when(() => weather.elevation).thenReturn(elevation);
-//         when(() => weather.currentWeather).thenReturn(currentWeather);
-//         when(
-//           () => weatherApi.getWeather(
-//             lat: any(named: 'lat'),
-//             long: any(named: 'long'),
-//           ),
-//         ).thenAnswer((_) async => weather);
+        final actual = await weatherRepository.getWeather(
+            latitude: latitude, longitude: longitude);
 
-//         final actual = await weatherRepository.getWeather(
-//             latitude: latitude, longitude: longitude);
+        expect(
+          actual,
+          const Weather(
+            temperature: temperature,
+            condition: WeatherCondition.clear,
+            latitude: latitude,
+            longitude: longitude,
+          ),
+        );
+      });
 
-//         expect(
-//           actual,
-//           const Weather(
-//             temperature: temperature,
-//             condition: WeatherCondition.cloudy,
-//             latitude: latitude,
-//             longitude: longitude,
-//           ),
-//         );
-//       });
-//     });
-//   });
-// }
+      test('returns correct weather on success (cloudy)', () async {
+        final weather = MockWeather();
+        final currentWeather = MockCurrentWeather();
+        when(() => currentWeather.temperature).thenReturn(temperature);
+        when(() => currentWeather.weathercode).thenReturn(2);
+        when(() => weather.latitude).thenReturn(latitude);
+        when(() => weather.longitude).thenReturn(longitude);
+        when(() => weather.elevation).thenReturn(elevation);
+        when(() => weather.currentWeather).thenReturn(currentWeather);
+        when(() => weatherApi.getWeather(any()))
+            .thenAnswer((_) async => weather);
+
+        final actual = await weatherRepository.getWeather(
+            latitude: latitude, longitude: longitude);
+
+        expect(
+          actual,
+          const Weather(
+            temperature: temperature,
+            condition: WeatherCondition.cloudy,
+            latitude: latitude,
+            longitude: longitude,
+          ),
+        );
+      });
+    });
+  });
+}
